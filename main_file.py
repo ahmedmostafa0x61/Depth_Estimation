6f277591ff67250dd8b693ff7585b3cf604b2919
Mazori <a.mostafa.0x61@gmail.com>
2021-07-24 18:43:40 +0200

    Added New Features

--- main_file.py
+++ main_file.py
@@ -1,7 +1,8 @@
-"""Compute depth maps for images in the input folder.
+"""Desktop app to compute depth map for a selected image.
 """
 import os
 import glob
+from shutil import copy2
 import torch
 import utils
 import cv2
@@ -23,8 +24,8 @@ from PyQt5.uic import loadUiType
 import sys
 
 from app_ui import Ui_MainWindow as Program
-
-# Program, _ = loadUiType('app_ui.ui')
+# Or use the following
+# Program, _ = loadUiType('app_ui2.ui')
 
 
 class MainApp(QMainWindow, Program):
@@ -32,9 +33,27 @@ class MainApp(QMainWindow, Program):
         super(MainApp, self).__init__()
         QMainWindow.__init__(self)
         self.setupUi(self)
-        self.showNormal()
+        self.model_initialization()
+        self.showMaximized()
+        self.user_action()
+        self.button_shortcuts()
+
+    def button_shortcuts(self):
+        self.select_pb.setShortcut("Ctrl+o")
+        self.convert_pb.setShortcut("Ctrl+r")
+        self.save_pb.setShortcut("Ctrl+s")
+
+    def user_action(self):
         self.select_pb.clicked.connect(self.select)
-        self.convert_pb.clicked.connect(self.convert)
+        self.convert_pb.clicked.connect(self.write_depth_ori)
+        self.save_pb.clicked.connect(self.save_image)
+        self.select_acc.currentIndexChanged.connect(self.model_initialization)
+
+    def button_activate(self, button):
+        if button == 'convert':
+            self.convert_pb.setEnabled(True)
+        elif button == 'save':
+            self.save_pb.setEnabled(True)
 
     def select(self):
         """
@@ -42,26 +61,27 @@ class MainApp(QMainWindow, Program):
         """
         global imagePath
         fname = QFileDialog.getOpenFileName(self, 'Open file')
-
         imagePath = fname[0]
         pixmap = QPixmap(imagePath)
         self.img_before.setPixmap(QPixmap(pixmap))
+        self.button_activate('convert')
 
-    def convert(self, optimize=True):
-        """
-        Build the model and Convert the image to its Depth
-        """
-        global imagePath
+    def model_initialization(self):
+        global transform, device, model, optimize
 
         # initialization
+        optimize = True
         model_type = "None"
         model_path = "None"
-        print(self.select_acc.currentText())
-        if self.select_acc.currentText() == 'High':
+        # set torch options
+        torch.backends.cudnn.enabled = True
+        torch.backends.cudnn.benchmark = True
+
+        if self.select_acc.currentText() == 'Option 1':
             """DPT-large model"""
             model_type = "dpt_large"
             model_path = "weights/dpt_large-midas-2f21e586.pt"
-        elif self.select_acc.currentText() == 'Low':
+        elif self.select_acc.currentText() == 'Option 2':
             '''midas v2.1 small '''
             model_type = "midas_v21_small"
             model_path = "weights/midas_v21_small-70d6b9c8.pt"
@@ -111,7 +131,7 @@ class MainApp(QMainWindow, Program):
 
         model.eval()
 
-        if optimize == True:
+        if optimize:
             # rand_example = torch.rand(1, 3, net_h, net_w)
             # model(rand_example)
             # traced_script_module = torch.jit.trace(model, rand_example)
@@ -123,9 +143,10 @@ class MainApp(QMainWindow, Program):
 
         model.to(device)
 
-        # get input
-        # create output folder
-        # os.makedirs(output_path, exist_ok=True)
+    def convert(self):
+        """
+        Build the model and Convert the image to its Depth
+        """
 
         print("start processing")
 
@@ -152,193 +173,59 @@ class MainApp(QMainWindow, Program):
                     .numpy()
             )
 
-        output_path = os.getcwd()
+        return prediction
+
+    def write_depth_ori(self):
+        global pic_path
+        prediction = self.convert()
+        # to avoid dir is already exists
+        try:
+            os.mkdir('records')
+        except FileExistsError:
+            pass
+
+        # Saving the Depth map into an image
+
+        # get current dir
+        current_path = os.getcwd()
+        record_path = os.path.join(current_path, 'records')
+        # get the last number in the output dir
+        # to set the next number to the image to be saved
+        converted_dir = len(os.listdir(record_path)) + 1
+        output_path = os.path.join(record_path, str(converted_dir))
+        # make a new dir to hold both the original and the converted image
+        try:
+            os.mkdir(output_path)
+        except FileExistsError:
+            converted_dir += 1
+            output_path = os.path.join(record_path, str(converted_dir))
+
+        # save the converted Image
         filename = os.path.join(
             output_path, os.path.splitext(os.path.basename(imagePath))[0]
         )
         pic_path = utils.write_depth(filename, prediction, bits=2)
 
+        # Copy the original Image
+        copy2(imagePath, filename + '.png')
+
+        # Show the Depth Map into QWidget
         qpic = QPixmap(pic_path)
         self.img_after.setPixmap(qpic)
+        self.button_activate('save')
 
+    def save_image(self):
 
-# Original code main function
-def run(input_path, output_path, model_path, model_type="large", optimize=True):
-    """Run MonoDepthNN to compute depth maps.
-
-    Args:
-        input_path (str): path to input folder
-        output_path (str): path to output folder
-        model_path (str): path to saved model
-    """
-    print("initialize")
-
-    # select device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("device: %s" % device)
-
-    # load network
-    if model_type == "dpt_large":  # DPT-Large
-        model = DPTDepthModel(
-            path=model_path,
-            backbone="vitl16_384",
-            non_negative=True,
-        )
-        net_w, net_h = 384, 384
-        resize_mode = "minimal"
-        normalization = NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-    elif model_type == "dpt_hybrid":  # DPT-Hybrid
-        model = DPTDepthModel(
-            path=model_path,
-            backbone="vitb_rn50_384",
-            non_negative=True,
-        )
-        net_w, net_h = 384, 384
-        resize_mode = "minimal"
-        normalization = NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-    elif model_type == "midas_v21":
-        model = MidasNet(model_path, non_negative=True)
-        net_w, net_h = 384, 384
-        resize_mode = "upper_bound"
-        normalization = NormalizeImage(
-            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-        )
-    elif model_type == "midas_v21_small":
-        model = MidasNet_small(model_path, features=64, backbone="efficientnet_lite3", exportable=True,
-                               non_negative=True, blocks={'expand': True})
-        net_w, net_h = 256, 256
-        resize_mode = "upper_bound"
-        normalization = NormalizeImage(
-            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-        )
-    else:
-        print(f"model_type '{model_type}' not implemented, use: --model_type large")
-        assert False
-
-    transform = Compose(
-        [
-            Resize(
-                net_w,
-                net_h,
-                resize_target=None,
-                keep_aspect_ratio=True,
-                ensure_multiple_of=32,
-                resize_method=resize_mode,
-                image_interpolation_method=cv2.INTER_CUBIC,
-            ),
-            normalization,
-            PrepareForNet(),
-        ]
-    )
-
-    model.eval()
-
-    if optimize == True:
-        # rand_example = torch.rand(1, 3, net_h, net_w)
-        # model(rand_example)
-        # traced_script_module = torch.jit.trace(model, rand_example)
-        # model = traced_script_module
-
-        if device == torch.device("cuda"):
-            model = model.to(memory_format=torch.channels_last)
-            model = model.half()
-
-    model.to(device)
-
-    # get input
-    img_names = glob.glob(os.path.join(input_path, "*"))
-    num_images = len(img_names)
-
-    # create output folder
-    os.makedirs(output_path, exist_ok=True)
-
-    print("start processing")
-
-    for ind, img_name in enumerate(img_names):
-
-        print("  processing {} ({}/{})".format(img_name, ind + 1, num_images))
-
-        # input
-
-        img = utils.read_image(img_name)
-        img_input = transform({"image": img})["image"]
-
-        # compute
-        with torch.no_grad():
-            sample = torch.from_numpy(img_input).to(device).unsqueeze(0)
-            if optimize == True and device == torch.device("cuda"):
-                sample = sample.to(memory_format=torch.channels_last)
-                sample = sample.half()
-            prediction = model.forward(sample)
-            prediction = (
-                torch.nn.functional.interpolate(
-                    prediction.unsqueeze(1),
-                    size=img.shape[:2],
-                    mode="bicubic",
-                    align_corners=False,
-                )
-                    .squeeze()
-                    .cpu()
-                    .numpy()
-            )
-
-        # output
-        filename = os.path.join(
-            output_path, os.path.splitext(os.path.basename(img_name))[0]
-        )
-        utils.write_depth(filename, prediction, bits=2)
-
-    print("finished")
+        name = QFileDialog.getSaveFileName(self, 'Save File')[0]
+        print(name + '.png')
+        copy2(pic_path, name + '.png')
 
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()
-    #
-    # parser.add_argument('-i', '--input_path',
-    #                     default='input',
-    #                     help='folder with input images'
-    #                     )
-    #
-    # parser.add_argument('-o', '--output_path',
-    #                     default='output',
-    #                     help='folder for output images'
-    #                     )
-    #
-    # parser.add_argument('-m', '--model_weights',
-    #                     default=None,
-    #                     help='path to the trained weights of model'
-    #                     )
-    #
-    # parser.add_argument('-t', '--model_type',
-    #                     default='dpt_large',
-    #                     help='model type: dpt_large, dpt_hybrid, midas_v21_large or midas_v21_small'
-    #                     )
-    #
-    # parser.add_argument('--optimize', dest='optimize', action='store_true')
-    # parser.add_argument('--no-optimize', dest='optimize', action='store_false')
-    # parser.set_defaults(optimize=True)
-    #
-    # args = parser.parse_args()
-    #
-    # default_models = {
-    #     "midas_v21_small": "weights/midas_v21_small-70d6b9c8.pt",
-    #     "midas_v21": "weights/midas_v21-f6b98070.pt",
-    #     "dpt_large": "weights/dpt_large-midas-2f21e586.pt",
-    #     "dpt_hybrid": "weights/dpt_hybrid-midas-501f0c75.pt",
-    # }
-    #
-    # if args.model_weights is None:
-    #     args.model_weights = default_models[args.model_type]
-
-    # set torch options
-    torch.backends.cudnn.enabled = True
-    torch.backends.cudnn.benchmark = True
-
-    # compute depth maps
-    # run(args.input_path, args.output_path, args.model_weights, args.model_type, args.optimize)
-
     # Run Desktop app
     app = QApplication(sys.argv)
     window = MainApp()
+    window.setWindowTitle('Arabesque')
+    # window.setWindowIcon(QIcon('icon.ico'))
     window.show()
     app.exec_()
